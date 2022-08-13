be59105b26484998120d91dbb8407a8be8cb927c
Paul Cuéllar <pacuellarp@unal.edu.co>
2022-08-13 05:58:12 -0500

    Path trial

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 	const initialState= useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
+			<BrowserRouter basename='/'>
 				<Layout>
 					<Routes>
 						<Route exact path="/" element={<Home/>} />
